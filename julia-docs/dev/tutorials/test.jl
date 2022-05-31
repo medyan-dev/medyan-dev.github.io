3d0b28ed13e084910d9cd2a7cf49cab87d8b545e
Documenter.jl <documenter@juliadocs.github.io>
2022-05-31 09:18:18 +0000

    build based on 520d472

--- julia-docs/dev/tutorials/test.jl
+++ julia-docs/dev/tutorials/test.jl
@@ -27,7 +27,7 @@ begin
 	elseif parentdir=="build"
 		Pkg.develop(path="../../../")
 	else
-Pkg.add(url="https://github.com/medyan-dev/MEDYAN.jl", rev="858e23e")
+Pkg.add(url="https://github.com/medyan-dev/MEDYAN.jl", rev="520d472")
 	end
 
 	
