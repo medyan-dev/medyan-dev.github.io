995def773eca7ead384ce7b5e40d1af9313d2cf0
Documenter.jl <documenter@juliadocs.github.io>
2022-07-04 15:59:46 +0000

    build based on ecefbdb

--- julia-docs/dev/tutorials/test.jl
+++ julia-docs/dev/tutorials/test.jl
@@ -27,7 +27,7 @@ begin
 	elseif parentdir=="build"
 		Pkg.develop(path="../../../")
 	else
-Pkg.add(url="git@github.com:medyan-dev/MEDYAN.jl.git", rev="1e5f92c")
+Pkg.add(url="git@github.com:medyan-dev/MEDYAN.jl.git", rev="ecefbdb")
 	end
 
 	
